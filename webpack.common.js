99e5c5f908fe0aba7a4444f0d9f3f540959d468a
JohannH <johann.hertz2009@gmail.com>
2026-06-20 11:54:23 -0400

    renewed the webpack configurations and split files:     dev     prod     for mor orginization

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,14 @@
 
 import path from "node:path";
+import { fileURLToPath } from 'node:url';
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 export default {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
